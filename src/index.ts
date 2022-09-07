488d05b92f7b95709f035fb84ff678995b852063
Vladislav <flickervladislav@gmail.com>
2022-09-07 18:03:41 +0300

    refactor: update eslint config

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,11 @@
-import { config } from './config'
-import { createServer } from './server'
-import { users } from './data/users'
-import { createProcesses } from './cluster'
+import { config } from './config';
+import { createServer } from './server';
 
-const PORT = config.PORT || 3000
-const HOSTNAME = config.HOST_NAME || 'localhost'
-const isMulti = config.MULTI
+const PORT = config.PORT || 3000;
+const HOSTNAME = config.HOST_NAME || 'localhost';
 
-const workerHandler = () => {
-  server.listen(PORT, HOSTNAME, () => {
-    console.log(`Listening on port on port: ${PORT}`)
-    console.log(`PID process: ${process.pid}`)
-  })
-}
+const server = createServer();
 
-const server = createServer(users)
-if (isMulti) {
-  createProcesses(workerHandler)
-} else {
-  server.listen(PORT, HOSTNAME, () => console.log(`Listening on port: ${PORT}`))
-}
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`Listening on port: http://${HOSTNAME}:${PORT}`);
+});
